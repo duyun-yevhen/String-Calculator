630215239ab7a89deb3eb4a5a1bd22d20086dd58
y.duiun <yevhen.duiun@ukad-group.com>
2021-06-15 19:51:24 +0300

    test?

--- StringCalculatorTest/ConsoleTests.cs
+++ StringCalculatorTest/ConsoleTests.cs
@@ -3,23 +3,28 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using Xunit;
+using Moq;
 
 namespace ConsoleApp.Test
 {
-
-
 	public class ConsoleTests
 	{
 		[Fact]
 		public void Console_OutputMessegesTest()
 		{
+			int i = 0;
 			// arrange
 			string[] expected = new string[] { "Enter comma separated numbers (enter to exit):", "Result: 43", "Another input please (enter to exit):" };
 			string[] nums = new string[] { "10, 33", String.Empty};
-			Program.console = new ConsoleMock(nums);
+			List<string> actual = new List<string>();
+
+			var moq = new Mock<IConsole>();
+			moq.Setup(a => a.ReadLine()).Returns(() => { return nums[i++]; });
+			moq.Setup(a => a.WriteLine(It.IsAny<string>())).Callback<string>((a)=> actual.Add(a));
+
+			Program.console = moq.Object;
 			// act
 			Program.Main(null);
-			var actual = ((ConsoleMock)Program.console).consoleList;
 			// assert
 			Assert.Equal(expected, actual);
 		}
